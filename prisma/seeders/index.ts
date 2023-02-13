f84a1dda07a4af45de4bf93e85f17fc5afe14e19
Fambegbe Olamileke <olamileke.f@gmail.com>
2023-02-14 00:15:49 +0100

    chore: implemented database seeders

---
+++ prisma/seeders/index.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client';
+import { main } from './category';
+
+const prisma = new PrismaClient();
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit();
+  });
