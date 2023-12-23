b824241e9bfe1e0b70e6c0284bcfdcbe6b1f665c
Fambegbe Olamileke <olamileke.f@gmail.com>
2023-12-24 00:01:27 +0100

    feat: added ping api route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UnguardedTokenRoute } from './utilities';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UnguardedTokenRoute()
+  @Post('/ping')
+  @HttpCode(200)
+  ping() {
+    return { status: 'ok' };
   }
 }
